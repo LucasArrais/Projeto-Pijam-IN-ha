9826abdeb609127d6f45d127f0298b431d6a35b0
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-03 23:34:06 +0100

    feat: criação do make use case do create do pijama

---
+++ BackEnd/src/use-cases/factories/pijamas/make-create-pijama-use-case.ts
@@ -0,0 +1,10 @@
+import { PrismaPijamasRepository } from "@/repositories/prisma/pijamas-prisma-repository.js";
+import { CreatePijamaUseCase } from "@/use-cases/pijamas/create-pijama-use-case.js";
+
+export function makeCreatePijamaUseCase() {
+    const pijamasRepository = new PrismaPijamasRepository()
+
+    const createPijamaUseCase = new CreatePijamaUseCase(pijamasRepository)
+
+    return createPijamaUseCase
+}
