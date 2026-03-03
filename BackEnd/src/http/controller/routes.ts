7ae2969af89e513d565512bb9ee72f7b180d8f8c
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-03-03 16:27:46 -0300

    chore: estruturando rotas

--- BackEnd/src/http/controller/routes.ts
+++ BackEnd/src/http/controller/routes.ts
@@ -1,5 +1,8 @@
 import type { FastifyInstance } from "fastify"
+import { AddressRoutes } from "./address/address.routes.js"
 
 export async function appRoutes(app:FastifyInstance) {
+
+    app.register(AddressRoutes, { prefix: '/address'})
     //app.register(usersRoutes, { prefix:'/users'}) como exemplo
 }
