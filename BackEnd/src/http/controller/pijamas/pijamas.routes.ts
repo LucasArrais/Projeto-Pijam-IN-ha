315d3d51b75f5b789665dac2a535ea8a4bc9fde9
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-04 11:29:01 +0100

    feat(/controller): adiciona rota para obter pijama individual

--- BackEnd/src/http/controller/pijamas/pijamas.routes.ts
+++ BackEnd/src/http/controller/pijamas/pijamas.routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { createPijama } from "./create-pijama.controller.js";
 import { updatePijama } from "./update-pijama.controller.js";
+import { getPijama } from "./get-pijama.controller.js";
 
 export async function pijamasRoutes(app: FastifyInstance) {
     app.post('/', createPijama)
     app.patch('/:publicId', updatePijama)
+    app.get('/:publicId', getPijama)
 }
---
+++ BackEnd/src/use-cases/factories/pijamas/make-get-pijama-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaPijamasRepository } from "@/repositories/prisma/pijamas-prisma-repository.js";
+import { GetPijamaUseCase } from "@/use-cases/pijamas/get-pijama-use-case.js";
+
+export function makeGetPijamaUseCase() {
+    const pijamasRepository = new PrismaPijamasRepository()
+    const getPijamaUseCase = new GetPijamaUseCase(pijamasRepository)
+
+    return getPijamaUseCase
+}
