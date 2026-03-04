da79b918c4139b8792e675413dc12d2430569ed1
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-04 10:22:15 +0100

    feat: criação do erro de ResourceNotFound e do use case do update pijama

---
+++ BackEnd/src/use-cases/pijamas/update-pijama-use-case.ts
@@ -0,0 +1,38 @@
+import type { PijamasRepository } from "@/repositories/pijamas-repository.js"
+import { ESTACAO, GENERO, TIPO, type Pijama } from "@/@types/prisma/client.js"
+import { ResourceNotFoundError } from "../errors/resource-not-found-error.js"
+
+interface UpdatePijamaUseCaseRequest {
+    publicId: string,
+    name?: string
+    description?: string
+    image?: string
+    price?: number
+    season?: ESTACAO
+    type?: TIPO
+    gender?: GENERO
+    on_sale?: boolean
+    sale_percent?: number | null
+}
+
+type UpdatePijamaUseCaseResponse = {
+    pijama: Pijama
+}
+
+export class UpdatePijamaUseCase {
+    constructor (private pijamasRepository: PijamasRepository) {}
+
+    async execute(request: UpdatePijamaUseCaseRequest): Promise<UpdatePijamaUseCaseResponse> {
+        const { publicId, ...data } = request;
+
+        const pijamaToUpdate = await this.pijamasRepository.findBy({publicId})
+
+        if(!pijamaToUpdate){
+            throw new ResourceNotFoundError()
+        }
+
+        const pijama = await this.pijamasRepository.update(pijamaToUpdate.id, data)
+
+        return { pijama }
+    }
+}
