f2a818a3cc50f139969b75de4eb65bc2470c3ae0
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-04 10:36:40 +0100

    feat(/controller): adiciona rota para atualizar pijama

--- BackEnd/src/http/controller/pijamas/pijamas.routes.ts
+++ BackEnd/src/http/controller/pijamas/pijamas.routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { createPijama } from "./create-pijama.controller.js";
+import { updatePijama } from "./update-pijama.controller.js";
 
 export async function pijamasRoutes(app: FastifyInstance) {
     app.post('/', createPijama)
+    app.patch('/:publicId', updatePijama)
 }
