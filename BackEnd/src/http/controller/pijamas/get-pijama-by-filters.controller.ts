9feb05dbead6e57f3a05c39c2e638d21b96d42a7
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-03-04 18:59:06 +0100

    feat(/controller): adiciona rota para filtrar pijamas por parâmetros

---
+++ BackEnd/src/http/controller/pijamas/get-pijama-by-filters.controller.ts
@@ -0,0 +1,27 @@
+import { ESTACAO, GENERO, TIPO } from "@/@types/prisma/enums.js";
+import { PijamaPresenter } from "@/http/presenters/pijamas-presenter.js";
+import { makeGetPijamasByFiltersUseCase } from "@/use-cases/factories/pijamas/make-get-pijamas-by-filters-use-case.js";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from 'zod'
+
+export async function getPijamasByFilters(request: FastifyRequest, reply: FastifyReply){
+    const queryParams = z.object({
+        season: z.enum(ESTACAO).optional(),
+        type: z.enum(TIPO).optional(),
+        gender: z.enum(GENERO).optional()
+    })
+
+    const { season, type, gender } = queryParams.parse(request.query)
+
+    const getPijamasByFilters = makeGetPijamasByFiltersUseCase()
+
+    const { pijamas } = await getPijamasByFilters.execute({
+        season,
+        type,
+        gender
+    })
+
+    return reply.status(200).send(
+        PijamaPresenter.toHTTP(pijamas)
+    )
+}
--- BackEnd/src/http/controller/pijamas/pijamas.routes.ts
+++ BackEnd/src/http/controller/pijamas/pijamas.routes.ts
@@ -4,12 +4,18 @@ import { updatePijama } from "./update-pijama.controller.js";
 import { getPijama } from "./get-pijama.controller.js";
 import { deletePijama } from "./delete-pijama.controller.js";
 import { listPijama } from "./list-pijamas.controller.js";
+import { getPijamasByFilters } from "./get-pijama-by-filters.controller.js";
 
 export async function pijamasRoutes(app: FastifyInstance) {
     app.post('/', createPijama)
     app.get('/:publicId', getPijama)
     app.get('/', listPijama) 
-    //ver se não deve ainda criar rotas de leitura para o tipo/genero/estacao
+    
     app.patch('/:publicId', updatePijama)
     app.delete('/:publicId', deletePijama)
+
+    app.get('/filter', getPijamasByFilters)
 }
+
+//se quiserem usar o filter, fazer: /pijamas/filter?gender=MASCULINO ou 
+// /pijamas/filter?type=... ou /pijamas/filter?season=...
